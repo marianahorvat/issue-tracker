1bfe7ce8bb72def9f99b9b50bea8660a32eb4b92
Mariana Horvat <mariana.horvat.qa@gmail.com>
2020-01-05 18:53:57 -0500

    New issue title component

--- src/components/NewIssue/Title.js
+++ src/components/NewIssue/Title.js
@@ -6,31 +6,29 @@ class Title extends Component {
   render() {
     return (
       <div className="Title">
-        <section id="page" class="title">
-          <div class="container">
-            <div class="row center-xs center-sm center-md center-lg">
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <h2><span class="primary-text">Add</span> New Issue</h2>
-                <p>Please use this form to submit a new issue</p>
-                <form>
-                  <div>
-                    <label for="name">Name</label><br />
-                    <input type="text" name="name" />
-                  </div>
-                  <div>
-                    <label for="email">Email</label><br />
-                    <input type="text" name="email" />
-                  </div>
-                  <div>
-                    <label for="message">Message</label><br />
-                    <textarea name="message"></textarea>
-                  </div>
-                  <button type="submit" name="button">Submit</button>
-                </form>
-              </div>
+        <div>
+          <div className="row">
+            <div class="col-xs-1">
+              <span className="bug-icon"><i className="fa fa-bug" aria-hidden="true"></i></span>
+            </div>
+            <div class="col-xs-3">
+              <input type="text" className="defect-number" name="defect-number" placeholder="Defect number" />
+            </div>
+            <div class="col-xs-7">
+              <input type="text" className="defect-title" name="defect-title" placeholder="Defect title" />
+            </div>
+            <div class="col-xs-1">
+              <span className="close-icon"><i className="fa fa-close" aria-hidden="true"></i></span>
             </div>
           </div>
-        </section>
+        </div>
+        <div className="row center-xs">
+          <div class="col-xs-7">
+              <button className="btn" type="submit" name="button"><i class="fa fa-caret-down"></i>Details</button>
+              <button className="btn" type="submit" name="button"><i class="fa fa-history"></i>Revision History</button>
+              <button className="btn" type="submit" name="button"><i class="fa fa-history"></i>Duplicates</button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/NewIssue.js
+++ src/pages/NewIssue.js
@@ -10,10 +10,24 @@ class NewIssue extends Component {
   render() {
     return (
       <div className="NewIssue">
-        <Title />
-        <Description />
-        <Details />
-        <Save />
+        
+        <section id="page" class="title">
+          <div class="container">
+            <div class="row center-xs center-sm center-md center-lg">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h2><span class="primary-text">Add</span> New Issue</h2>
+                <p>Please use this form to submit a new issue</p>
+                <form>
+                  <Title />
+                  <Description />
+                  <Details />
+                  <Save />
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </div>
     );
   }
